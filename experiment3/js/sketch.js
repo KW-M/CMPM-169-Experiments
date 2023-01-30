e12f9369025f858692f1b456eb3a7e9cd27ac246
Kyle Worcester-Moore <25kylworc@gmail.com>
2023-01-29 20:36:22 -0800

    experiment 3 start

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -2,66 +2,218 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// Inspired by: https://openprocessing.org/sketch/1769288
+// Inspired by: https://openprocessing.org/sketch/1752310
+
+// CONSTS
+let START_REGION = { top: 0, bottom: 0, left: 0, right: 0 }
+let START_REGION_SIZE = 100;
+let PARTICLE_COUNT = 2000;
+let PARTICLE_COLORS = [];
+let PARTICLE_SPEED = 10;
+let RANDOM_JITTER = 0.0;
+let NOISESCALE = 0.01;
+let NOISEOFFSET = 3992;
+
+let CENTERX = 0;
+let CENTERY = 0;
+
+let GRID_CELL_SIZE = 20;
+let gridZones;
 
 // Globals
-let myInstance;
 let canvasContainer;
+let particles = [];
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+function getNoise(x, y) {
+    return noise(x * NOISESCALE + NOISEOFFSET, y * NOISESCALE - NOISEOFFSET)
+}
+
+
+class gridZone {
+    gridCells = [];
+    width = 0;
+    height = 0;
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+        this.gridCells = Array(Math.ceil(width / GRID_CELL_SIZE * height / GRID_CELL_SIZE));
     }
 
-    myMethod() {
-        // code to run when method is called
+    getIndex(x, y) {
+        y = Math.ceil(y);
+        x = Math.ceil(x);
+        if (x < 0 || x > this.width || y < 0 || y > this.height) console.warn("Invalid grid index x,y : ", x, y, this.width, this.height);
+        return Math.floor(y / GRID_CELL_SIZE) * Math.ceil(this.width / GRID_CELL_SIZE) + Math.floor(x / GRID_CELL_SIZE);
     }
+
+    getValues(x, y) {
+        return this.gridCells[this.getIndex(x, y)] || [];
+    }
+
+    add(index, value) {
+        const cell = this.gridCells[index] || [];
+        cell.push(value);
+        this.gridCells[index] = cell;
+        return index;
+    }
+
+    remove(index, value) {
+        const cell = this.gridCells[index];
+        if (cell === undefined || cell.length === 0) return index;
+        const valueIndex = cell.indexOf(value);
+        if (valueIndex != -1) { this.gridCells[index].splice(valueIndex, 1); }
+        return index;
+    }
+
+    move(index, x, y, value) {
+        const newIndex = this.getIndex(x, y)
+        if (newIndex === index) return index;
+        this.remove(index, value)
+        this.add(newIndex, value)
+        return newIndex;
+    }
+
+    getAllCells() {
+        return this.gridCells
+    }
+}
+
+class particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.aliveTime = 0;
+        this.bounds = { top: y, bottom: y, left: x, right: x }
+        this.gridZoneIndex = -5;
+    }
+
+    reset() {
+        this.x = random(START_REGION.left, START_REGION.right);
+        this.y = random(START_REGION.top, START_REGION.bottom);
+        this.aliveTime = 0;
+        this.bounds = { top: this.y, bottom: this.y, left: this.x, right: this.x }
+    }
+
+    updateBounds() {
+        if (this.x < this.bounds.left) {
+            this.bounds.left = this.x;
+        } else if (this.x > this.bounds.right) {
+            this.bounds.right = this.x;
+        }
+        if (this.y < this.bounds.top) {
+            this.bounds.top = this.y;
+        } else if (this.y > this.bounds.bottom) {
+            this.bounds.bottom = this.y;
+        }
+    }
+
+    getBoundsArea() {
+        return (this.bounds.right - this.bounds.left) * (this.bounds.bottom - this.bounds.top);
+    }
+
+    update(timestep) {
+        this.aliveTime += timestep;
+        let noiseHere = getNoise(this.x, this.y);
+        if (this.x < 0 || this.x > width || this.y < 0 || this.y > height || noiseHere < 0.01 || noiseHere > 0.99) this.reset();
+        let mouseYNorm = mouseY / height;
+        let noiseGradientX = noiseHere - getNoise(this.x + 1, this.y);
+        let noiseGradientY = noiseHere - getNoise(this.x, this.y + 1);
+        let gradientVector = createVector(noiseGradientX, noiseGradientY).rotate((90 + (mouseYNorm - 0.5) * 20 - (noiseHere - 0.5) * 10) * Math.PI / 180) //(90 + (noiseHere - 0.5) * 100)
+        const JITTER = RANDOM_JITTER * this.aliveTime / (this.getBoundsArea() + 1);
+        let velocityX = gradientVector.x * PARTICLE_SPEED + random(JITTER, -JITTER);
+        let velocityY = gradientVector.y * PARTICLE_SPEED + random(JITTER, -JITTER);
+        this.x += velocityX * timestep;
+        this.y += velocityY * timestep;
+        this.updateBounds();
+        this.gridZoneIndex = gridZones.move(this.gridZoneIndex, this.x, this.y, this)
+        return noiseHere;
+
+    }
+}
+
+function preload() {
+
 }
 
 // setup() function is called once when the program starts
 function setup() {
+
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
+
     // resize canvas is the page is resized
-    $(window).on("resize", function () {
+    const resizeFunc = () => {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+        CENTERX = width / 2;
+        CENTERY = height / 2;
+        START_REGION = { top: CENTERY - START_REGION_SIZE, bottom: CENTERY + START_REGION_SIZE, left: CENTERX - START_REGION_SIZE, right: CENTERX + START_REGION_SIZE }
+    }; $(window).on("resize", resizeFunc)
+    resizeFunc();
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    // START_REGION = { top: -height / 2, bottom: height / 2, left: -width / 2, right: width / 2 };
+    PARTICLE_COLORS = [color(200, 10, 90), color(90, 80, 80), color(0, 100, 80), color(0, 110, 200)];
+    for (let i = 0; i < PARTICLE_COUNT; i++) {
+        particles.push(new particle())
+        particles[i].reset();
+    }
+
+    gridZones = new gridZone(width, height);
+
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+
 function draw() {
-    background(220);
-    // call a method on the instance
-    myInstance.myMethod();
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    translate(-CENTERX, -CENTERY);
+
+    for (let x = 0; x < Math.ceil(width / GRID_CELL_SIZE); x++) {
+        for (let y = 0; y < Math.ceil(height / GRID_CELL_SIZE); y++) {
+            const zone = gridZones.getValues(x * GRID_CELL_SIZE + 1, y * GRID_CELL_SIZE + 1)
+            if (zone.length != 0) {
+                fill(255, zone.length)
+            } else {
+                fill(0, 0)
+            }
+            rect(x * GRID_CELL_SIZE, y * GRID_CELL_SIZE, GRID_CELL_SIZE, GRID_CELL_SIZE)
+        }
+    }
+
+    stroke(255, 255)
+    rect(START_REGION.left, START_REGION.top, START_REGION.right - START_REGION.left, START_REGION.bottom - START_REGION.top)
+
+    stroke(0, 0)
+    for (let i = 0; i < PARTICLE_COUNT; i++) {
+        fill(PARTICLE_COLORS[i % PARTICLE_COLORS.length])
+        // fill(255, 255, 255, 255)
+        particles[i].update(deltaTime)
+        circle(particles[i].x, particles[i].y, 5)
+    }
+
+    // fade out gradually
+    if (frameCount % 2 === 0) {
+        console.log("CENTERX", CENTERX)
+        blendMode(SUBTRACT); // Workaround from here: https://stackoverflow.com/questions/6817729/gradual-fading-by-drawing-a-transparent-rectangle-repeatedly
+        fill(255, 3);
+        rect(0, 0, width, height);
+        blendMode(BLEND);
+    }
 }
+
+
+
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
     // code to run when mouse is pressed
+    mousePress = true;
+}
+
+function mouseReleased() {
+    // code to run when mouse is pressed
+    mousePress = false;
 }
--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -2,66 +2,235 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// CONSTS
+let fftBins = 250;
+
 
 // Globals
-let myInstance;
 let canvasContainer;
+let theShader;
+let fingersVideo;
+let cam;
+let camReady = false;
+let fft;
+let graphics
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
 
-    myMethod() {
-        // code to run when method is called
+
+let mousePress = false;
+
+function preload() {
+
+}
+
+function getGrayscalePixels(image) {
+    pixelDensity(1);
+    image.loadPixels();
+    let pixels = image.pixels;
+    let width = image.width;
+    let height = image.height;
+    let widthInArray = image.width * 4 // RGBA
+    let outpixels = new ArrayBuffer(width * height)
+    let counter = 0;
+    for (let i = 0; i < pixels.length; i += widthInArray) {  // rows
+        for (let j = i; j < i + widthInArray; j += 4) {  // pixels in row
+            let index = i + j;
+            ArrayBuffer[counter] = (pixels[index] /* r */ + pixels[index + 1] /* g */ + pixels[index + 2] /* b */) / 3
+            counter++;
+        }
     }
+    return outpixels
+}
+
+function writeGrayscaleImage(pixels, width, height) {
+    const g = createGraphics(width, height)
+    for (let y = 0; y < pixels.length; y += width) {  // rows
+        for (let x = y; x < y + width; x += 1) {  // pixels in row
+            let index = y + x;
+            g.set(x, y, pixels[index])
+        }
+    }
+    g.updatePixels()
+    return g;
+}
+
+const getPixelFactory = (image) => {
+    pixelDensity(1);
+    image.loadPixels();
+    let pixels = image.pixels;
+    return function (x, y) {
+        if (arguments.length === 0) return h_es;
+
+        const index = y * image.width * 4 + x * 4;
+        return (pixels[index] /* r */ + pixels[index + 1] /* g */ + pixels[index + 2] /* b */) / 3;
+    }
+}
+
+function forier2d(image) {
+    let width = image.width;
+    let height = image.height;
+    let getPixel = getPixelFactory(image)
+
+    // compute the h hat values
+    let h_hats = [];
+
+    Fourier.transform(getPixel, h_hats);
+    h_hats = Fourier.shift(h_hats, [width, height]);
+    // get the largest magnitude
+    // var maxMagnitude = 0;
+    // for (var ai = 0; ai < h_hats.length; ai++) {
+    //     var mag = h_hats[ai].magnitude();
+    //     if (mag > maxMagnitude) {
+    //         maxMagnitude = mag;
+    //     }
+    // }
+
+    return h_hats
 }
 
 // setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+
+    // video source
+    // fingersVideo = createVideo(['assets/fingers.mov', 'assets/fingers.webm']);
+    // fingersVideo.loop();
+
+    // // camera source
+    // cam = createCapture(VIDEO, () => { console.log(camReady); camReady = true });
+    // cam.size(canvasContainer.width() / 2, canvasContainer.height() / 2);
+    // cam.hide();
+
+    // canvasContainer.html("<h2>Allow camera to see demo 📹</h2>")
+
+    // let i_id = setInterval(() => {
+    //     if (!camReady) return;
+    //     clearInterval(i_id)
+    //     $("#canvas-container>h2").remove();
+    // }, 10)
+
+
+    // create a Fast forier transform:
+    fft = new p5.FFT();
+
+    // Create an Audio input
+    let mic = new p5.AudioIn()
+
+    // start the Audio Input.
+    // By default, it does not .connect() (to the computer speakers)
+    mic.start();
+    mic.connect(fft);
+
+    let image = loadImage("./assets/bigsur.png")
+    // let h_hats = forier2d(image);
+    let h_hats = new Array(wi)
+    graphics = writeGrayscaleImage(h_hats, image.width, image.height)
+
+
+
+    // for (let i = 0; i < d; i++) {  // rows
+    //     for (let j = 0; j < d; j++) {  // pixels in row
+    //         // loop over
+    //         let index = 4 * ((y * d + j) * width * d + (x * d + i));
+    //         (pixels[index] /* r */ + pixels[index + 1] /* g */ + pixels[index + 2] /* b */) / 3
+    //     }
+    // }
+
+
+
+    // place our canvas, making it fit our container
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
+
     // resize canvas is the page is resized
     $(window).on("resize", function () {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+let avg_spectrum = null
 function draw() {
-    background(220);
-    // call a method on the instance
-    myInstance.myMethod();
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    image(graphics, 0, 0, image.width, image.height)
+    return;
+
+    let topLeftX = -width / 2;
+    let topLeftY = -height / 2;
+    let mouseXNorm = map(mouseX, 0, width, 0, 1);
+    let mouseYNorm = map(mouseY, 0, height, 0, 1);
+    let angle = frameCount * Math.PI / 800;
+
+    let scanlineRadius = min(width, height) / 2;
+    let scanlineX = sin(angle) * scanlineRadius;
+    let scanlineY = cos(angle) * scanlineRadius;
+
+    let scanlineNormalX = sin(angle + (Math.PI / 2)) * 1;
+    let scanlineNormalY = cos(angle + (Math.PI / 2)) * 1;
+
+
+    // get forier transform of sound
+    let waveform = fft.waveform();
+    let spectrum = fft.analyze();
+    fftBins = spectrum.length
+    if (avg_spectrum == null) {
+        avg_spectrum = spectrum
+    }
+
+
+    // line(-scanlineX, -scanlineY, scanlineX, scanlineY)
+
+    const segmentPct = 1 / fftBins;
+    const startX = -scanlineX, startY = -scanlineY
+    let x = startX, y = startY;
+    const avgRatio = 0.005
+    for (let i = 0; i < fftBins; i++) {
+        avg_spectrum[i] = (avg_spectrum[i] * (1 - avgRatio) + spectrum[i] * avgRatio)
+        const wiggleOffset = random(waveform[i] * 40);
+        const newX = lerp(-scanlineX, scanlineX, i * segmentPct) + (scanlineNormalX - 0.5) * wiggleOffset
+        const newY = lerp(-scanlineY, scanlineY, i * segmentPct) + (scanlineNormalY - 0.5) * wiggleOffset
+        const centerGradient = 255 - abs(segmentPct * i - 0.5) * 255 * 2;
+        stroke(centerGradient / 2, (spectrum[i] - avg_spectrum[i]) * 5, centerGradient)
+        line(x, y, newX, newY)
+        x = newX;
+        y = newY;
+    }
+
+    // x = startX, y = startY;
+    // for (let i = 0; i < waveform.length; i++) {
+    //     const wiggleOffset = waveform[i] * 90;
+    //     const newX = lerp(-scanlineX, scanlineX, i * segmentPct) + (scanlineNormalX - 0.5) * wiggleOffset
+    //     const newY = lerp(-scanlineY, scanlineY, i * segmentPct) + (scanlineNormalY - 0.5) * wiggleOffset
+
+    //     stroke(255, 50);
+    //     line(x, y, newX, newY)
+    //     x = newX;
+    //     y = newY;
+    // }
+
+    // fade out gradually
+    if (frameCount % 3 == 0) {
+        blendMode(SUBTRACT); // Workaround from here: https://stackoverflow.com/questions/6817729/gradual-fading-by-drawing-a-transparent-rectangle-repeatedly
+        fill(255, 1);
+        rect(topLeftX, topLeftY, width, height);
+        blendMode(BLEND);
+    }
+
 }
+
+
+
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
     // code to run when mouse is pressed
+    mousePress = true;
+}
+
+function mouseReleased() {
+    // code to run when mouse is pressed
+    mousePress = false;
 }
